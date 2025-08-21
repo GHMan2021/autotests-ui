23bda3e00450b32d574800d0b2c8ee8d5eea4f4a
GHMan2021 <statio2009@gmail.com>
2025-08-21 09:46:15 +0400

    Логирование взаимодействий со страницей. Практика работы с логированием

---
+++ tools/playwright/mocks.py
@@ -0,0 +1,15 @@
+from playwright.sync_api import Page, Route
+
+
+def mock_static_resources(page: Page):
+    page.route("**/*.{ico,png,jpg,webp,mp3,mp4,woff,woff2}", lambda route: route.abort())
+
+# def abort(route: Route):
+#     # Временно печатаем ссылку на отключаемый ресурс
+#     print(f'\nAborting url: {route.request.url}')
+#     route.abort()  # Отменяем загрузку ресурса
+#
+#
+# def mock_static_resources(page: Page):
+#     # Отключаем загрузку статических ресурсов
+#     page.route("**/*.{ico,png,jpg,webp,mp3,mp4,woff,woff2}", abort)
