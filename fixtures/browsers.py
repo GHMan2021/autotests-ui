fdc984c07932b413881cf1142cf928293d468e50
GHMan2021 <statio2009@gmail.com>
2025-08-21 11:23:57 +0400

    Added run autotests on CI

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -3,9 +3,10 @@ import pytest
 from _pytest.fixtures import SubRequest
 from playwright.sync_api import Playwright, Page
 
-from config import settings
+from config import settings, Browser
 from pages.authentication.registration_page import RegistrationPage
 from tools.playwright.pages import initialize_playwright_page
+from tools.routes import AppRoute
 
 
 @pytest.fixture(params=settings.browsers)
@@ -14,18 +15,18 @@ def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
 
 
 @pytest.fixture(scope="session")
-def initialize_browser_state(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False)
+def initialize_browser_state(playwright: Playwright, browser_type: Browser,):
+    browser = playwright[browser_type].launch(headless=False)
     context = browser.new_context(base_url=settings.get_base_url())
     page = context.new_page()
 
     # Работаем с регистрационной страницей через Page Object
     registration_page = RegistrationPage(page=page)
-    registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+    registration_page.visit(AppRoute.REGISTRATION)
     registration_page.registration_form.fill(
-        email=settings.test_user.email,  # Используем settings.test_user.email
-        username=settings.test_user.username,  # Используем settings.test_user.username
-        password=settings.test_user.password  # Используем settings.test_user.password
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password
     )
     registration_page.click_registration_button()
 
